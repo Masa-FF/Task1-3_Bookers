977811539f06dfdbbbf5d0d2263c6b7b2a7d9504
EC2 Default User <ec2-user@ip-172-31-81-73.ec2.internal>
2020-10-25 07:36:39 +0000

    profile_image,create

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   validates :name, length: { maximum: 20 }
   validates :name, length: { minimum: 2 }
   validates :name, presence: true
+  
+  attachment :profile_image
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
