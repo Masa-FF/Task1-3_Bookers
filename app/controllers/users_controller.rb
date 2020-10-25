2fe264186ad82bb85ad4c4ebabdbdaa6c2217894
EC2 Default User <ec2-user@ip-172-31-81-73.ec2.internal>
2020-10-25 03:06:23 +0000

    Success_message,created

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  
+  def index
+    @users = User.all
+    @user = current_user
+  end
+  
+  def new
+    @user = User.new  
+  end  
+  
+  def edit
+    @user = User.find(params[:id]) 
+  end  
+
+  def show
+    @user = User.find(params[:id]) 
+  end
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, length: { maximum: 20 }
+  validates :name, length: { minimum: 2 }
+  validates :name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
