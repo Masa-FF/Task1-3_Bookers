dcd960f57a4dbe4a2101f7ef81383925e327f59d
EC2 Default User <ec2-user@ip-172-31-81-73.ec2.internal>
2020-10-26 11:07:39 +0000

    Rspec_test1

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources  :users, only: [:index, :show, :create, :edit, :update]
   resources :books#, only: [:new, :create, :index, :show]
   
-  get 'about' => 'home#about'
+  get 'home/about' => 'home#about'
   
   # patch 'users/:id' => 'users#update', as: 'update_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
