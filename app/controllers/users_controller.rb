3c2feb4f240c3e03b20955e9a2a4036c9b4855bf
EC2 Default User <ec2-user@ip-172-31-81-73.ec2.internal>
2020-10-27 08:25:18 +0000

    created_v1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id]) 
+    @user = User.find(params[:id])
+    if @user.id != current_user.id
+      redirect_to user_path(current_user)
+    end
   end  
 
   def show
